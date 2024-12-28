7300682599d1ad65ca9661ea78faf2a7409fc92b
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2024-12-29 00:09:51 +0900

    feat: add Benchmark

---
+++ sandbox/SandboxBenchmark/SelectionSortBenchmark.cs
@@ -0,0 +1,30 @@
+﻿using SortLab.Core;
+using SortLab.Core.Logics;
+
+namespace SandboxBenchmark;
+
+[ShortRunJob]
+[MemoryDiagnoser]
+[MinColumn, MaxColumn]
+public class SelectionSortBenchmark
+{
+    [Params(100, 1000)]
+    public int Number { get; set; }
+
+    private int[] _array = default!;
+    private CycleSort<int> _cycleSort = default!;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _array = Enumerable.Range(0, Number).Sample(Number).ToArray();
+
+        _cycleSort = new();
+    }
+
+    [Benchmark]
+    public void CycleSort()
+    {
+        _cycleSort.Sort(_array);
+    }
+}
