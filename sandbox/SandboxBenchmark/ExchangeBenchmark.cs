d6ee614d0f9283f58a18a6add823a18b06dcffe7
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2025-01-14 04:44:15 +0900

    chore: Benchmark BogoSort

---
+++ sandbox/SandboxBenchmark/ExchangeBenchmark.cs
@@ -0,0 +1,27 @@
+﻿namespace SandboxBenchmark;
+
+[ShortRunJob]
+[MemoryDiagnoser]
+[MinColumn, MaxColumn]
+public class ExchangeBogoBenchmark
+{
+    //[Params(10, 13)]
+    [Params(10)]
+    public int Number { get; set; }
+
+    private int[] _bogoArray = default!;
+    private BogoSort<int> _bogoSort = default!;
+
+    [IterationSetup]
+    public void Setup()
+    {
+        _bogoArray = BenchmarkData.GenIntArray(Number);
+        _bogoSort = new();
+    }
+
+    [Benchmark]
+    public void BogoSort()
+    {
+        _bogoSort.Sort(_bogoArray);
+    }
+}
