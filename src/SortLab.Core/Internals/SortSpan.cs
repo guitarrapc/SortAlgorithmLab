be07b7070bd4e4e5ad6460099d4dea74ccb92294
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2026-01-22 00:49:26 +0900

    feat: add SortSpan

---
+++ src/SortLab.Core/Internals/SortSpan.cs
@@ -0,0 +1,47 @@
+﻿using SortLab.Core.Contexts;
+
+namespace SortLab.Core.Internals;
+
+internal ref struct SortSpan<T>(Span<T> span, ISortContext ccontext) where T: IComparable<T>
+{
+    private Span<T> _span = span;
+    private readonly ISortContext _context = ccontext;
+
+    public int Length => _span.Length;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public T Read(int i)
+    {
+        _context.OnIndexAccess(i);
+        return _span[i];
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Write(int i, T value)
+    {
+        _context.OnIndexAccess(i);
+        _span[i] = value;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int Compare(int i, int j)
+    {
+        var a = Read(i);
+        var b = Read(j);
+        var result = a.CompareTo(b);
+        _context.OnCompare(i, j, result);
+        return result;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Swap(int i, int j)
+    {
+        var a = Read(i);
+        var b = Read(j);
+
+        _context.OnSwap(i, j);
+
+        Write(i, b);
+        Write(j, a);
+    }
+}
